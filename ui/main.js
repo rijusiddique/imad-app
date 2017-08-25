967bb428ce20709b3ccbe240a59f5d2217c670cd
Riju Siddique <rijusiddique@users.noreply.github.com>
2017-08-25 23:27:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,11 @@
 console.log('Loaded!');
 var img1 = document.getElementById('img1');
+var marginLeft = 0;
+function moveRight(){
+   marginLeft = marginLeft+10;
+   img1.style.marginLeft = marginLeft+'px';
+};
 img1.onclick=function(){
     alert("moving image");
-    img1.style.marginleft='200px';
+    var interval = setInterval(moveRight,100);
 };
